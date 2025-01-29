17934458cfa8541012d62f388575a694e9ab6977
Rohan Shanavas <34649755+rohanshanavas@users.noreply.github.com>
2025-01-29 11:55:28 +0000

    Added a REST API to test endpoint

--- src/main/java/dev/rohan/movies/MoviesApplication.java
+++ src/main/java/dev/rohan/movies/MoviesApplication.java
@@ -2,13 +2,23 @@ package dev.rohan.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+
+	@GetMapping("/")
+	public String rootAPI() {
+		return "Hello, Spring!";
+	}
+	
 
 }
